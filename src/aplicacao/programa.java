fa1bb92ade033f0a4a4166f6aaf9d00b2054b4a1
Tulio Gontijo <tulio_gontijo@hotmail.com>
2020-05-06 17:35:50 -0300

    Relatorio gerado

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -12,6 +12,7 @@ public class programa {
 
 		char sexo;
 		int qtdCerverja, qtdRefri, qtdEspeto;
+		double ingresso, couvert, consumo;
 
 		System.out.print("Sexo: ");
 		sexo = sc.next().charAt(0);
@@ -21,6 +22,30 @@ public class programa {
 		qtdRefri = sc.nextInt();
 		System.out.print("Quantidade de espeto: ");
 		qtdEspeto = sc.nextInt();
+		
+		consumo = (qtdCerverja * 5) + (qtdEspeto * 7) + (qtdRefri * 3);
+
+		if (sexo == 'F' || sexo == 'f') {
+			ingresso = 8;
+		} else {
+			ingresso = 10;
+		}
+		if (consumo >= 30) {
+			couvert = 0;
+		} else {
+			couvert = 4;
+		}
+
+		System.out.println("\nRELATORIO:");
+		System.out.printf("Consumo = R$ %.2f%n", consumo);
+		if (couvert == 0) {
+			System.out.println("Isento de Couvert");
+		} else {
+			System.out.printf("Couvert = R$ %.2f%n", couvert);
+		}
+		System.out.printf("Ingresso = R$ %.2f%n%n", ingresso);
+		System.out.printf("Valor a pagar = R$ %.2f%n", (consumo + couvert + ingresso));
+
 
 		sc.close();
 
