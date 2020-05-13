c4dad5e05741926dcb044899e289982ac3098af6
Tulio Gontijo <tulio_gontijo@hotmail.com>
2020-05-13 19:51:54 -0300

    Leitura dos outros dados

--- src/programa.java
+++ src/programa.java
@@ -7,15 +7,24 @@ public class programa {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int escolaridade;
+		int escolaridade, experiencia;
+		char viagem, cnh;
 		
 		System.out.println("Qual sua escolaridade?");
 		System.out.println("1) Ensino Fundamental");
 		System.out.println("2) Ensino Médio");
 		System.out.println("3) Ensino Superior");
 		System.out.println("4) Pós-graduação");
-		System.out.print("Digite uma opção:");
+		System.out.print("Digite uma opção: ");
 		escolaridade = sc.nextInt();
+		
+		System.out.println();
+		System.out.print("Você tem quantos anos de experiência profissional? ");
+		experiencia = sc.nextInt();
+		System.out.print("Você tem disponibilidade para viajar (S/N)? ");
+		viagem = sc.next().charAt(0);
+		System.out.print("Você tem habilitação de motorista (S/N)? ");
+		cnh = sc.next().charAt(0);
 		
 		sc.close();
 	}
