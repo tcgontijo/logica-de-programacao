6e7e8641ec55366a8c61d9f29a43d28c4ac804e8
Tulio Gontijo <tulio_gontijo@hotmail.com>
2020-05-15 19:20:56 -0300

    Cabecalho

---
+++ src/programa.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+
+public class programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		System.out.println("SISTEMA DE PERFIL DE CLIENTE");
+		System.out.println("--------------------------------");
+		System.out.println("INFORME OS DADOS DO ÚLTIMO ANO");
+
+	}
+
+}
